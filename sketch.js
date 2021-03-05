29f9cdda3acc381cd5d9fc8c0ce94baad307626f
shreyal0905 <78970130+shreyal0905@users.noreply.github.com>
2021-03-05 21:11:47 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var ground,box;
+
+function setup(){
+    var canvas = createCanvas(800,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    var ground_options ={
+        isStatic: true
+    }
+
+    var box_options={
+        restitution:1.0
+    }
+
+   ground= Bodies.rectangle(200,390,200,20,ground_options);
+    World.add(world,ground);
+    box=Bodies.rectangle(200,100,20,20,box_options)
+    World.add(world,box);
+
+   
+
+
+    console.log(ground);
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+    rectMode(CENTER);
+    rect(ground.position.x,ground.position.y,400,20);
+    rect(box.position.x,box.position.y,20,20)
+}
